c1fabf592895d31d1b1e285292d969de7215d7b6
NyitraiBalzs <nyitrai.bazs@gmail.com>
2022-03-30 11:05:22 +0200

    ADD addNewBoard event to boardsManager.js

--- static/js/controller/boardsManager.js
+++ static/js/controller/boardsManager.js
@@ -4,22 +4,38 @@ import {domManager} from "../view/domManager.js";
 import {cardsManager} from "./cardsManager.js";
 
 export let boardsManager = {
+
     loadBoards: async function () {
+        let currenBoardId = 0;
         const boards = await dataHandler.getBoards();
         for (let board of boards) {
             const boardBuilder = htmlFactory(htmlTemplates.board);
             const content = boardBuilder(board);
+            currenBoardId = board.id
             domManager.addChild("#root", content);
             domManager.addEventListener(
                 `.toggle-board-button[data-board-id="${board.id}"]`,
                 "click",
                 showHideButtonHandler
             );
+
         }
+        document.getElementById('add-board').dataset.current_board_id = `${currenBoardId}`
+        domManager.addEventListener(`#add-board[data-current_board_id="${currenBoardId}"]`,
+                                    'click',
+                                    addNewBoard)
     },
 };
 
 function showHideButtonHandler(clickEvent) {
     const boardId = clickEvent.target.dataset.boardId;
-    cardsManager.loadCards(boardId);
+    cardsManager.loadCards(boardId).then();
+}
+
+
+function  addNewBoard(clickEvent){
+    const boardId = clickEvent.target.dataset.current_board_id
+    const boardName = 'board ' + boardId
+    console.log(boardName)
+    dataHandler.createNewBoard(boardName).then()
 }
